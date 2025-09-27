443dd58f858d338358efa22fd47c83fe695a5c96
Nitin Rao <sg727427@gmail.com>
2025-09-27 14:49:38 +0530

    Fix: navbar import path issue

--- food_delivery/src/App.jsx
+++ food_delivery/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import Navbar from './components/navbar/navbar';
+import Navbar from './components/navbar/Navbar';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/home/home';
 import Cart from './pages/cart/cart';
