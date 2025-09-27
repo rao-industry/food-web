5a2c70eee427673ed79a2ddc906128ceab3065cb
Nitin Rao <sg727427@gmail.com>
2025-09-27 14:55:59 +0530

    Fix: corrected import paths (case sensitivity issue)

--- food_delivery/src/App.jsx
+++ food_delivery/src/App.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 import Navbar from './components/navbar/Navbar';
 import { Route, Routes } from 'react-router-dom';
-import Home from './pages/home/home';
-import Cart from './pages/cart/cart';
-import PlaceOrder from './pages/placeOrder/placeorder';
+import Home from './pages/Home/Home';
+import Cart from './pages/Cart/Cart';
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder';
 import Footer from './components/Footer/Footer';
 import LoginPopup from './components/LoginPopup/LoginPopup';
 import Verify from './pages/verify/verify';
